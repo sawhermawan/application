b719ec6b4f8cb58178a4503ff5078667aeaac255
sawhermawan <sayakeras@gmail.com>
2020-01-24 00:11:59 +0700

    modifikasi dashboard

--- views/admin/static.php
+++ views/admin/static.php
@@ -115,7 +115,7 @@
           <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu_section">
               <ul class="nav side-menu">
-                <li><a href="<?php echo base_url();?>index.php/welcome"><i class="fa fa-home"></i>Home</a></li>
+                <li><a href="<?php echo base_url();?>index.php/dashboard"><i class="fa fa-home"></i>Home</a></li>
                 <!-- Dashboard Master -->
                 <li><a><i class="fa fa-share"></i> Master <span class="fa fa-chevron-down"></span></a>
                   <ul class="nav child_menu">
